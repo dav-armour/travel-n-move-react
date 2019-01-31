a3627a253e6d40f6ee0ad841d2b21f5d933647f7
Santosh <santosh.poudyal@gmail.com>
2019-01-31 12:47:32 +1100

    grabbed action from action>index, put action to mapStateToprops, destructured action to getTour function and match, passed id to getTourfunction to hit the API, grabbed the data which is available in props> tour

--- src/components/pages/TourShowPage.js
+++ src/components/pages/TourShowPage.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getTour } from "./../../actions";
 
 class TourShowPage extends Component {
+  componentDidMount() {
+    const { getTour, match } = this.props;
+    const { id } = match.params;
+    getTour(id);
+  }
   render() {
-    console.log(this.props);
-    return <div>ID: {this.props.match.params.id}</div>;
+    const { tour } = this.props;
+    console.log("tour:", tour);
+    return <div>{tour.title}</div>;
+
+    // ) <div>ID: {this.props.match.params.id}</div>;
   }
 }
 
-export default TourShowPage;
+const mapStateToProps = state => {
+  return {
+    tour: state.tour
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getTour }
+)(TourShowPage);
