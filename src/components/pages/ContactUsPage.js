a62df811427c6bf4cfd257e37a673f9b45ced509
Santosh <santosh.poudyal@gmail.com>
2019-02-01 15:49:09 +1100

    fixed cards in Contact Us page to make them responsive

--- src/components/pages/ContactUsPage.js
+++ src/components/pages/ContactUsPage.js
@@ -9,8 +9,30 @@ import { FormLabel } from "@material-ui/core";
 
 const styles = theme => ({
   paper: {
-    flexGrow: 1
+    flexGrow: 1,
+    minHeight: 390,
+    // minWidth: 400,
+    maxWidth: 410
     // minWidth: 300
+  },
+  mainWrapper: {
+    marginTop: "40px"
+  },
+  contactWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    // justifyContent: "center"
+    // justifyContent: "center",
+    alignItems: "flex-start",
+    paddingLeft: "30px",
+    paddingTop: 15
+  },
+  contactRows: {
+    // border: "1px solid black",
+    marginBottom: "10px"
+  },
+  rowTitle: {
+    fontWeight: "bold"
   }
 });
 
@@ -30,20 +52,73 @@ class ContactUsPage extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.mainWrapper}>
         <Grid container spacing={24} justify="center">
-          <Grid item xs={12} sm={6} md={4}>
+          <Grid item xs={10} sm={6} md={4}>
             <Paper className={classes.paper}>
               <FormContainer>
                 <ContactUsForm />
               </FormContainer>
             </Paper>
           </Grid>
-          <Grid container xs={12} sm={6} md={4}>
+          <Grid item xs={10} sm={6} md={4}>
             <Paper className={classes.paper}>
-              {/* <FormContainer> */}
-              {/* <FormLabel>Our Contact</FormLabel> */}
-              {/* </FormContainer> */}
+              <div className={classes.contactWrapper}>
+                <Typography
+                  variant="h5"
+                  gutterBottom
+                  className={classes.rowTitle}
+                >
+                  Our Details
+                </Typography>
+                <div className={classes.contactRows}>
+                  <Typography variant="button" gutterBottom>
+                    Registered Office:
+                  </Typography>
+
+                  <Typography variant="subheading" gutterBottom>
+                    Indira Colony,Lane No:5, Pathankot, Punjab, 145001
+                  </Typography>
+                </div>
+                <div className={classes.contactRows}>
+                  <Typography variant="button" gutterBottom>
+                    Branch Office:
+                  </Typography>
+
+                  <Typography variant="subheading" gutterBottom>
+                    Shop No:13, First Floor, Gurjit Market, Dhangu Road,
+                    Pathankot, Punjab, 145001
+                  </Typography>
+                </div>
+                <div className={classes.contactRows}>
+                  <Typography variant="button" gutterBottom>
+                    Mobile:
+                  </Typography>
+
+                  <Typography variant="subheading" gutterBottom>
+                    89687-05332
+                  </Typography>
+                </div>
+
+                <div className={classes.contactRows}>
+                  <Typography variant="button" gutterBottom>
+                    Landline:
+                  </Typography>
+                  <Typography variant="subheading" gutterBottom>
+                    0186-2225332
+                  </Typography>
+                </div>
+
+                <div className={classes.contactRows}>
+                  <Typography variant="button" gutterBottom>
+                    Email:
+                  </Typography>
+
+                  <Typography variant="subheading" gutterBottom>
+                    info@travelnmove.com, travelnmove1@gmail.com
+                  </Typography>
+                </div>
+              </div>
             </Paper>
           </Grid>
         </Grid>
