699c005e3ada3162d1589cb52ee9e6231eece56e
Santosh <santosh.poudyal@gmail.com>
2019-02-03 21:47:29 +1100

    change in about us

--- src/components/pages/AboutUsPage.js
+++ src/components/pages/AboutUsPage.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import HeroImage from "./../modules/HeroImage";
 import { withStyles } from "@material-ui/core/styles";
+import classes from "*.module.css";
+
+const styles = theme => {
+  aboutUsWrapper: {
+  }
+};
 
 class AboutUsPage extends Component {
   render() {
-    return;
-    <div>
-      <HeroImage imageLink="./londonEye.jpg" height={"40vh"} />
-      <div />
-    </div>;
+    return (
+      <div>
+        <HeroImage imageLink="./londonEye.jpg" height={"40vh"} />
+        <div className={classes.aboutUsWrapper} />
+      </div>
+    );
   }
 }
 
